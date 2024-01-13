51d949b46ee3436516c8fcd903830a718f1d6d98
Luis Bazaldua <Luis29178@gmail.com>
2024-01-12 19:13:11 -0600

    Remove .env from tracking

--- app/(modal)/locationSearch.tsx
+++ app/(modal)/locationSearch.tsx
@@ -4,19 +4,15 @@ import MapView from 'react-native-maps'
 import Colors from '@/constants/Colors'
 import { useNavigation } from 'expo-router'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { Ionicons } from '@expo/vector-icons'
+
+
 
 //google API key
 // process.env.EXPO_PUBLIC_GOOGLE_API_KEY
 
 const locationSearch = () => {
 
-
-
-    useEffect(() => {
-        console.log(process.env.EXPO_PUBLIC_GOOGLE_API_KEY);
-    }, [])
-
-    
     const navigation = useNavigation();
     const [location, setlocation] = useState({
         latitude: 29.897285,
@@ -29,17 +25,50 @@ const locationSearch = () => {
         <View style={{ flex: 1 }}>
 
             <GooglePlacesAutocomplete
-            placeholder="Search" 
-            onPress={(data, details = null) => {
-                    console.log(data, details)
-            }} 
-            query={{
-                key: process.env.EXPO_PUBLIC_GOOGLE_API_KEY,
-                language:'en',
-            }}
+                placeholder="Find a location"
+                textInputProps={{
+                    placeholderTextColor: Colors.medium,
+                }}
+                fetchDetails={true}
+                onPress={(data, details) => {
+                    const point = details?.geometry?.location
 
-            
-            
+                    if (point === undefined) return;
+
+                    setlocation({
+                        ...location,
+                        latitude: point.lat,
+                        longitude: point.lng,
+                    })
+
+                }}
+                query={{
+                    key: process.env.EXPO_PUBLIC_GOOGLE_API_KEY,
+                    language: 'en',
+                }}
+
+                styles={{
+                    container: {
+                        flex: 0,
+                    },
+                    textInput: {
+                        backgroundColor: Colors.grey,
+                        paddingLeft: 35,
+                        borderRadius: 10,
+
+                        placeholderTextColor: "#000000"
+                    },
+                    textInputContainer: {
+                        backgroundColor: "#ffffff",
+                        padding: 8,
+
+                    }
+                }}
+
+                renderLeftButton={() => <View style={styles.seachIcon}>
+                    <Ionicons name='search-outline' size={24} color={Colors.medium}/>
+                </View>}
+
             />
 
             <MapView showsUserLocation={true} style={styles.map} region={location} ></MapView>
@@ -86,6 +115,13 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: 16,
 
+
+    },
+    seachIcon: {
+        zIndex: 10,
+        position: 'absolute',
+        left:15,
+        top: 16,
 
     }
 
